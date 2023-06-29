c418b3ba3d1ece1f5688ee293dc48b46f71be205
Masaya1582 <masa8y88@gmail.com>
2023-06-29 19:58:56 +0900

    Initial Commit

---
+++ Xib_Playground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xib_Playground
+//
+//  Created by MasayaNakakuki on 2023/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
