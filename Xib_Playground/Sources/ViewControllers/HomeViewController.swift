76ff479f6acce771cc93e7fadfc90a1232a8be6d
Masaya1582 <masa8y88@gmail.com>
2023-06-29 20:31:59 +0900

    feat: CustomTableViewの実装方法

--- Xib_Playground/Sources/ViewControllers/HomeViewController.swift
+++ Xib_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -11,9 +11,41 @@ import RxCocoa
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+
+    private let fruitsArray = ["りんご", "みかん", "バナナ", "ぶどう", "メロン", "スイカ", "マンゴー"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello World")
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomTableViewCell")
+    }
+
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    // セクション内のRowの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruitsArray.count
+    }
+
+    // セルに表示させたいものの設定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell", for: indexPath) as? CustomTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configure(fruitsArray[indexPath.row])
+        return cell
+    }
+
+    // 高さ
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.view.frame.height / 8
     }
 
 }
