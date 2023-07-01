26ba1dccbf4f986be762a39fdbee2145c90255db
Masaya1582 <masa8y88@gmail.com>
2023-07-01 19:53:53 +0900

    feat: ネットワーク接続確認のライブラリ

--- Xib_Playground/Sources/ViewControllers/HomeViewController.swift
+++ Xib_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import Reachability
 
 class HomeViewController: UIViewController {
     // MARK: - Dependency
     typealias Dependency = Void
 
     // MARK: - Properties
+    @IBOutlet private weak var networkTypeLabel: UILabel!
+
     private let disposeBag = DisposeBag()
     // private let viewMoel = <#ViewModel名#>
 
@@ -28,6 +31,20 @@ class HomeViewController: UIViewController {
 // MARK: - Bindings
 private extension HomeViewController {
     func bind(to dependency: Dependency) {
-
+        do {
+            let reachability = try Reachability()
+            switch reachability.connection {
+            case .wifi:
+                networkTypeLabel.text = "Wi-Fi に接続しています"
+            case .cellular:
+                networkTypeLabel.text = "キャリアの回線に接続しています"
+            case .unavailable:
+                networkTypeLabel.text = "インターネットに接続されていません"
+            case .none:
+                networkTypeLabel.text = "該当なし"
+            }
+        } catch {
+            networkTypeLabel.text = "エラーが発生しました"
+        }
     }
 }
