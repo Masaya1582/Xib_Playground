93e913eb601d8d14165ca2ca82fc8f8da552b464
Masaya1582 <masa8y88@gmail.com>
2023-07-02 16:58:06 +0900

    feat: Map上に点を打つ

--- Xib_Playground/Sources/ViewControllers/HomeViewController.swift
+++ Xib_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -8,19 +8,40 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import MapKit
 
 class HomeViewController: UIViewController {
     // MARK: - Dependency
     typealias Dependency = Void
 
     // MARK: - Properties
+    @IBOutlet private weak var mapView: MKMapView!
+
     private let disposeBag = DisposeBag()
     // private let viewMoel = <#ViewModel名#>
 
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupMapView()
         bind(to: ())
+    }
+
+    // MapViewの初期設定
+    private func setupMapView() {
+        let tapGestureRecognizer = UITapGestureRecognizer()
+        tapGestureRecognizer.addTarget(self, action: #selector(onTap(sender:)))
+        mapView.addGestureRecognizer(tapGestureRecognizer)
+        mapView.frame = view.bounds
+        mapView.delegate = self
+        view.addSubview(mapView)
+    }
+
+    @objc func onTap(sender: UITapGestureRecognizer) {
+        let tapPoint = sender.location(in: mapView)
+        let location = mapView.convert(tapPoint, toCoordinateFrom: mapView)
+        let circle = MKCircle(center: location, radius: 10000)
+        mapView.addOverlay(circle)
     }
 
 }
@@ -30,4 +51,19 @@ private extension HomeViewController {
     func bind(to dependency: Dependency) {
 
     }
+}
+
+// MapViewDelegateの記述
+extension HomeViewController: MKMapViewDelegate {
+    public func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let circle = overlay as? MKCircle {
+            let circleRenderer = MKCircleRenderer(circle: circle)
+            circleRenderer.strokeColor = .black
+            circleRenderer.fillColor = .black
+            circleRenderer.lineWidth = 1.5
+            return circleRenderer
+        }
+        return MKOverlayRenderer()
+    }
+
 }
