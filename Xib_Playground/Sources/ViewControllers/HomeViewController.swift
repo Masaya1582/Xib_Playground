f3cc6c59ad0312f92bea7b0a659e4a2ac4330afb
Masaya1582 <masa8y88@gmail.com>
2023-07-03 15:04:24 +0900

    feat: TextField内の入力文字数をリアルタイムで反映

--- Xib_Playground/Sources/ViewControllers/HomeViewController.swift
+++ Xib_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -14,13 +14,54 @@ class HomeViewController: UIViewController {
     typealias Dependency = Void
 
     // MARK: - Properties
+    @IBOutlet private weak var nameField: UITextField!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var addressField: UITextField!
+    @IBOutlet private weak var addressLabel: UILabel!
+
+    // 名前フィールドの上限文字数
+    private let maxNameFieldSize = 10
+    // 住所フィールドの上限文字数
+    private let maxAddressFieldSize = 50
+
+    let limitText: (Int) -> String = {
+        return "あと\($0)文字"
+    }
     private let disposeBag = DisposeBag()
     // private let viewMoel = <#ViewModel名#>
 
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTextField()
         bind(to: ())
+    }
+
+    // TextFieldの初期設定
+    private func setupTextField() {
+        nameField.addTarget(self, action: #selector(nameFieldEditingChanged(sender:)), for: .editingChanged)
+        addressField.addTarget(self, action: #selector(addressFieldEditingChanged(sender:)), for: .editingChanged)
+    }
+
+    // "あと何文字"の部分をリアルタイムで検知して値を変更する
+    @objc func nameFieldEditingChanged(sender: UITextField) {
+        guard let changeText = sender.text else {return}
+        let limitCount = maxNameFieldSize - changeText.count
+        if limitCount < 1 {
+            nameField.isEnabled = false
+        }
+        nameField.isEnabled = true
+        nameLabel.text = limitText(limitCount)
+    }
+    // 同上
+    @objc func addressFieldEditingChanged(sender: UITextField) {
+        guard let changeText = sender.text else {return}
+        let limitCount = maxAddressFieldSize - changeText.count
+        if limitCount < 1 {
+            addressField.isEnabled = false
+        }
+        addressField.isEnabled = true
+        addressLabel.text = limitText(limitCount)
     }
 
 }
