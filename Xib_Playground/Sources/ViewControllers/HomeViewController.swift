a84f5cece0b983b8e030a0a7facb64696a131daf
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-07 08:51:16 +0900

    HalfAlertの表示

--- Xib_Playground/Sources/ViewControllers/HomeViewController.swift
+++ Xib_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -14,6 +14,9 @@ final class HomeViewController: UIViewController {
     typealias Dependency = Void
 
     // MARK: - Properties
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var showHalfAlertButton: UIButton!
+
     private let disposeBag = DisposeBag()
     private let viewModel: Dependency
 
@@ -34,12 +37,49 @@ final class HomeViewController: UIViewController {
         bind(to: viewModel)
     }
 
+    private func showHalfAlertView() {
+        // UIAlertControllerを用意する
+        let actionAlert = UIAlertController(title: "Fish or Beef", message: "どちらかを選んでください", preferredStyle: UIAlertController.Style.actionSheet)
+
+        // Fishを選択したときのアクション
+        let kabigonAction = UIAlertAction(title: "Fish", style: UIAlertAction.Style.default, handler: {
+            (action: UIAlertAction!) in
+            self.label.text = "Fish Please"
+            print("Fishを選択")
+        })
+        actionAlert.addAction(kabigonAction)
+
+        // Beefを選択したときのアクション
+        let pikachuAction = UIAlertAction(title: "Beef", style: UIAlertAction.Style.default, handler: {
+            (action: UIAlertAction!) in
+            self.label.text = "Beef Please"
+            print("Beefを選択")
+        })
+        actionAlert.addAction(pikachuAction)
+
+        // Cancelを選択したときのアクション
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: {
+            (action: UIAlertAction!) in
+            self.label.text = "キャンセルしました"
+            print("Cancelします")
+        })
+        actionAlert.addAction(cancelAction)
+
+        // アラート表示
+        self.present(actionAlert, animated: true, completion: nil)
+
+    }
+
 }
 
 // MARK: - Bindings
 private extension HomeViewController {
     func bind(to viewModel: Dependency) {
-
+        showHalfAlertButton.rx.tap.asSignal()
+            .emit(onNext: { [weak self] in
+                self?.showHalfAlertView()
+            })
+            .disposed(by: disposeBag)
     }
 }
 
