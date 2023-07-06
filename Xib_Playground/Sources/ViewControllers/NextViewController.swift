333352e9d499266146b055209f297f1e9dde3bc4
Masaya1582 <masa8y88@gmail.com>
2023-07-06 10:01:05 +0900

    Xib開発のモーダルとナビゲーション遷移

---
+++ Xib_Playground/Sources/ViewControllers/NextViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NextViewController.swift
+//  Xib_Playground
+//
+//  Created by MasayaNakakuki on 2023/07/06.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
