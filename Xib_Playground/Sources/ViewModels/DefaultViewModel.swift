bc3100babdde74c03642b1a2174e1d1f44be108b
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-07 17:10:20 +0900

    RxSwift, InputとOutputの良い理解？

--- Xib_Playground/Sources/ViewModels/DefaultViewModel.swift
+++ Xib_Playground/Sources/ViewModels/DefaultViewModel.swift
@@ -9,11 +9,11 @@ import RxSwift
 import RxCocoa
 
 protocol DefaultViewModelInputs: AnyObject {
-
+    var updateButtonTapped: PublishRelay<Void> { get }
 }
 
 protocol DefaultViewModelOutputs: AnyObject {
-
+    var numberText: BehaviorRelay<String> { get }
 }
 
 protocol DefaultViewModelType: AnyObject {
@@ -24,7 +24,10 @@ protocol DefaultViewModelType: AnyObject {
 class DefaultViewModel: DefaultViewModelType, DefaultViewModelInputs, DefaultViewModelOutputs {
 
     // MARK: - Input Sources
+    let updateButtonTapped = PublishRelay<Void>()
+
     // MARK: - Output Sources
+    let numberText = BehaviorRelay<String>(value: "Tap the button")
 
     // MARK: - Properties
     var inputs: DefaultViewModelInputs { return self }
@@ -33,7 +36,13 @@ class DefaultViewModel: DefaultViewModelType, DefaultViewModelInputs, DefaultVie
     private let disposeBag = DisposeBag()
 
     init() {
-        
+        updateButtonTapped
+            .subscribe(onNext: { [weak self] in
+                guard let self else { return }
+                let randomNumber = Int.random(in: 1...100)
+                self.numberText.accept("Random Number: \(randomNumber)")
+            })
+            .disposed(by: disposeBag)
     }
 
 }
