97fc6f3a656f34a1ca3f1c6150c42632f6c1f1c3
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-18 16:43:07 +0900

    緯度経度を取得する

--- Xib_Playground/Sources/ViewControllers/ViewController.swift
+++ Xib_Playground/Sources/ViewControllers/ViewController.swift
@@ -6,12 +6,54 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+
+    @IBOutlet private weak var locationLabel: UILabel!
+
+    private var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+
+    private func getCurrentLocation() {
+        // 緯度、経度表示用のラベル
+        locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        switch status {
+        case .restricted, .denied:
+            print("It's denied")
+        case .notDetermined:
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager?.startUpdatingLocation()
+        default:
+            break
+        }
+    }
+
+    @IBAction func getCurrentLocationAction(_ sender: Any) {
+        getCurrentLocation()
+    }
+
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    // 緯度経度が変更された時に呼ばれる
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        guard let latitude = location?.coordinate.latitude else { return }
+        guard let longitude = location?.coordinate.longitude else { return }
+        print("Latitude: \(latitude)\nLongitude: \(longitude)")
+        locationLabel.text = "Latitude: \(latitude)\nLongitude: \(longitude)"
     }
 
 }
