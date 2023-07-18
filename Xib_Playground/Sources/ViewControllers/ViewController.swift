a77fb2f66411d5a0ddf24270dec589b8e0f5fd5b
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-18 16:29:07 +0900

    ランダム値を取得

--- Xib_Playground/Sources/ViewControllers/ViewController.swift
+++ Xib_Playground/Sources/ViewControllers/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var randomLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+
+    @IBAction func randomAction(_ sender: Any) {
+        // ランダム値を取得するための定数を用意
+        guard let randomPrefecture = PrefectureData.prefectureArray.randomElement() else { return }
+        // ボタンが押された時にラベルのテキストを随時変更する
+        randomLabel.text = "\(String(describing: randomPrefecture))に行こう"
     }
 
 }
