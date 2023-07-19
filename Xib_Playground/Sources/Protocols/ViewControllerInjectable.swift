e8aeb64a993d6ff41e58d6f46fdeb3b87fd5d5fb
Masaya Nakakuki <masa8y88@gmail.com>
2023-07-19 16:34:52 +0900

    ViewControllerInjectableのリファクタ

--- Xib_Playground/Sources/Protocols/ViewControllerInjectable.swift
+++ Xib_Playground/Sources/Protocols/ViewControllerInjectable.swift
@@ -13,3 +13,9 @@ protocol ViewControllerInjectable: UIViewController {
 
     init(dependency: Dependency)
 }
+
+extension ViewControllerInjectable where Dependency == Void {
+    init(dependency: Dependency = ()) {
+        self.init(dependency: dependency)
+    }
+}
