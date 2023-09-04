8190b854be86660ae02b666a2a4cfc67d6dcbf38
Masaya Nakakuki <masa8y88@gmail.com>
2023-09-05 08:28:51 +0900

    シンプルにログインロジックを作る

--- Xib_Playground/Sources/ViewControllers/HomeViewController.swift
+++ Xib_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -11,9 +11,12 @@ import RxCocoa
 
 final class HomeViewController: UIViewController {
     // MARK: - Dependency
-    typealias Dependency = Void
+    typealias Dependency = HomeViewModelType
 
     // MARK: - Properties
+    @IBOutlet private weak var nameTextField: UITextField!
+    @IBOutlet private weak var loginButton: DesignableButton!
+
     private let disposeBag = DisposeBag()
     private let viewModel: Dependency
 
@@ -34,16 +37,37 @@ final class HomeViewController: UIViewController {
         bind(to: viewModel)
     }
 
+    private func showAlerView() {
+        let alert = UIAlertController(title: "Login", message: "Login Successful!", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            print("【はい】が押された")
+        })
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
+
 }
 
 // MARK: - Bindings
 private extension HomeViewController {
-    func bind(to viewModel: Dependency) {
-//        <#Button#>.rx.tap.asSignal()
-//            .emit(onNext: { [weak self] in
-//                <#Actions#>
-//            })
-//            .disposed(by: disposeBag)
+    func bind(to viewModel: HomeViewModelType) {
+
+        nameTextField.rx.text.orEmpty
+            .bind(to: viewModel.inputs.nameInput)
+            .disposed(by: disposeBag)
+
+        viewModel.outputs.loginEnabled
+            .drive(onNext: { [weak self] loginEnabled in
+                self?.loginButton.isEnabled = loginEnabled
+                self?.loginButton.backgroundColor = loginEnabled ? .systemIndigo : .lightGray
+            })
+            .disposed(by: disposeBag)
+
+        loginButton.rx.tap.asSignal()
+            .emit(onNext: { [weak self] in
+                self?.showAlerView()
+            })
+            .disposed(by: disposeBag)
     }
 }
 
