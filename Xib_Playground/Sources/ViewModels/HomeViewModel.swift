9c47c2e5f5da9c3d7290b191228a7721de69b6c0
Masaya Nakakuki <masa8y88@gmail.com>
2023-10-13 13:43:19 +0900

    RxSwift,TextFieldリアルタイム反映のデフォルト

--- Xib_Playground/Sources/ViewModels/HomeViewModel.swift
+++ Xib_Playground/Sources/ViewModels/HomeViewModel.swift
@@ -10,9 +10,13 @@ import RxCocoa
 import Action
 
 protocol HomeViewModelInputs: AnyObject {
+    var idInput: PublishRelay<String> { get }
+    var passwordInput: PublishRelay<String> { get }
 }
 
 protocol HomeViewModelOutputs: AnyObject {
+    var idOutput: Driver<String> { get }
+    var passwordOutput: Driver<String> { get }
 }
 
 protocol HomeViewModelType: AnyObject {
@@ -26,12 +30,33 @@ final class HomeViewModel: HomeViewModelType, HomeViewModelInputs, HomeViewModel
     var outputs: HomeViewModelOutputs { return self }
 
     // MARK: - Input Sources
+    var idInput = PublishRelay<String>()
+    var passwordInput = PublishRelay<String>()
     // MARK: - Output Sources
+    var idOutput: Driver<String>
+    var passwordOutput: Driver<String>
 
+    private let _idOutput = BehaviorRelay<String>(value: "")
+    private let _passwordOutput = BehaviorRelay<String>(value: "")
     private let disposeBag = DisposeBag()
 
     // MARK: - Initialize
     init() {
+        idInput
+            .map { "ID: \($0)" }
+            .bind(to: _idOutput)
+            .disposed(by: disposeBag)
+
+        idOutput = _idOutput
+            .asDriver(onErrorJustReturn: "")
+
+        passwordInput
+            .map { "Password: \($0)" } 
+            .bind(to: _passwordOutput)
+            .disposed(by: disposeBag)
+
+        passwordOutput = _passwordOutput
+            .asDriver(onErrorJustReturn: "")
     }
 
 }
