f6418097c6353331535c93101b7bee603df1c655
Masaya1582 <masa8y88@gmail.com>
2023-10-14 09:39:43 +0900

    TextFieldのInputのProgressをLabelで更新

--- Xib_Playground/Sources/ViewModels/HomeViewModel.swift
+++ Xib_Playground/Sources/ViewModels/HomeViewModel.swift
@@ -10,9 +10,15 @@ import RxCocoa
 import Action
 
 protocol HomeViewModelInputs: AnyObject {
+    var nameInput: PublishRelay<String> { get }
+    var birthdayInput: PublishRelay<String> { get }
+    var idInput: PublishRelay<String> { get }
+    var passwordInput: PublishRelay<String> { get }
 }
 
 protocol HomeViewModelOutputs: AnyObject {
+    var isOKButtonEnabled: Driver<Bool> { get }
+    var progressLabel: Driver<String> { get }
 }
 
 protocol HomeViewModelType: AnyObject {
@@ -26,12 +32,34 @@ final class HomeViewModel: HomeViewModelType, HomeViewModelInputs, HomeViewModel
     var outputs: HomeViewModelOutputs { return self }
 
     // MARK: - Input Sources
+    var nameInput = PublishRelay<String>()
+    var birthdayInput = PublishRelay<String>()
+    var idInput = PublishRelay<String>()
+    var passwordInput = PublishRelay<String>()
     // MARK: - Output Sources
+    let isOKButtonEnabled: Driver<Bool>
+    let progressLabel: Driver<String>
 
+    private let filledTextFieldsCount = BehaviorRelay<Int>(value: 0)
     private let disposeBag = DisposeBag()
 
     // MARK: - Initialize
     init() {
+        Observable.combineLatest(nameInput, birthdayInput, idInput, passwordInput)
+            .map { inputs in
+                return [inputs.0, inputs.1, inputs.2, inputs.3].reduce(0) { count, input in
+                    return input.isEmpty ? count : count + 1
+                }
+            }
+            .bind(to: filledTextFieldsCount)
+            .disposed(by: disposeBag)
+
+        isOKButtonEnabled = filledTextFieldsCount.map { $0 == 4 }.asDriver(onErrorJustReturn: false)
+        progressLabel = filledTextFieldsCount.map { count in
+            let progress = count * 25
+            return "Progress: \(progress)%"
+        }
+        .asDriver(onErrorJustReturn: "Progress: 0%")
     }
 
 }
