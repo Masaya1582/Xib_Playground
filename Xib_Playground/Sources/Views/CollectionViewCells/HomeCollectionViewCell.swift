320f29dedceda1912b518caae9938e6d825395fe
Masaya Nakakuki <masa8y88@gmail.com>
2023-10-17 13:56:32 +0900

    Add PressAnimationButton

--- Xib_Playground/Sources/Views/CollectionViewCells/HomeCollectionViewCell.swift
+++ Xib_Playground/Sources/Views/CollectionViewCells/HomeCollectionViewCell.swift
@@ -2,8 +2,7 @@
 //  HomeCollectionViewCell.swift
 //  Xib_Playground
 //
-//  Created by 中久木 雅哉(Nakakuki Masaya) on 2023/10/12.
-//  Copyright (c) 2023 ReNKCHANNEL. All rihgts reserved.
+//  Created by MasayaNakakuki on 2023/06/29.
 //
 
 import UIKit
---
+++ Xib_Playground/Sources/Views/DesignableComponents/PressableMaterialButton.swift
@@ -0,0 +1,82 @@
+//
+//  PressAnimationButton.swift
+//  Xib_Playground
+//
+//  Created by MasayaNakakuki on 2023/06/29.
+//
+
+import UIKit
+
+class PressAnimationButton: UIButton {
+
+    private var shadowOffsetHeight: CGFloat = 5
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+
+    // MARK: - Configure UI
+    private func configureUI() {
+        self.layer.cornerRadius = self.frame.height / 2
+        addShadow(with: .white)
+    }
+
+    private func addShadow(with color: UIColor) {
+        self.layer.shadowOffset = CGSize(width: 0, height: shadowOffsetHeight)
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowRadius = 1.0
+        self.layer.shadowOpacity = 1.0
+    }
+}
+
+// MARK: - Touches methods
+extension PressAnimationButton {
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        pressedButtonAnimation()
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        releasedButtonAnimation()
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        releasedButtonAnimation()
+    }
+
+    private func pressedButtonAnimation() {
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: .curveEaseIn) {
+            self.transform = CGAffineTransform(translationX: 0, y: self.shadowOffsetHeight)
+        }
+        addShadowHeightAnimation(from: self.shadowOffsetHeight, to: 0, option: .easeIn)
+    }
+
+    private func releasedButtonAnimation() {
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: .curveEaseOut) {
+            self.transform = CGAffineTransform.identity
+        }
+        addShadowHeightAnimation(from: 0, to: self.shadowOffsetHeight, option: .easeOut)
+    }
+
+    private func addShadowHeightAnimation(from fromValue: CGFloat, to toValue: CGFloat, option: CAMediaTimingFunctionName) {
+        let animation = CABasicAnimation(keyPath: "shadowOffset")
+        animation.duration = 0.1
+        animation.fromValue = CGSize(width: 0, height: fromValue)
+        animation.toValue = CGSize(width: 0, height: toValue)
+        animation.timingFunction = CAMediaTimingFunction(name: option)
+        animation.autoreverses = false
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        self.layer.add(animation, forKey: nil)
+    }
+
+}
