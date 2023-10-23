282746d64635877aa170b70d67ba88d015785abb
Masaya Nakakuki <masa8y88@gmail.com>
2023-10-23 13:49:06 +0900

    初心者課題: ログインロジックを作る

--- Xib_Playground/Sources/ViewControllers/HomeViewController.swift
+++ Xib_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -13,7 +13,18 @@ final class HomeViewController: UIViewController {
     // MARK: - Dependency
     typealias Dependency = Void
 
+    enum AlertType {
+        case success
+        case error(String) 
+    }
+
     // MARK: - Properties
+    @IBOutlet private weak var idLabel: UILabel!
+    @IBOutlet private weak var passwordLabel: UILabel!
+    @IBOutlet private weak var idTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginButton: DesignableButton!
+
     private let disposeBag = DisposeBag()
     private let viewModel: Dependency
 
@@ -34,16 +45,39 @@ final class HomeViewController: UIViewController {
         bind(to: viewModel)
     }
 
+    private func showAlert(type: AlertType) {
+        let alert: UIAlertController
+        switch type {
+        case .success:
+            alert = UIAlertController(title: "Success", message: "ログイン成功", preferredStyle: .alert)
+        case .error(let message):
+            alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        }
+
+        let close = UIAlertAction(title: "Close", style: .default, handler: { [weak self] (action) -> Void in
+            self?.dismiss(animated: true)
+        })
+        alert.addAction(close)
+        self.present(alert, animated: true, completion: nil)
+    }
+
 }
 
 // MARK: - Binding
 private extension HomeViewController {
     func bind(to viewModel: Dependency) {
-//        <#Button#>.rx.tap.asSignal()
-//            .emit(onNext: { [weak self] in
-//                <#Actions#>
-//            })
-//            .disposed(by: disposeBag)
+        loginButton.rx.tap.asSignal()
+            .emit(onNext: { [weak self] in
+                guard let self = self else { return }
+                if self.idTextField.text == "" || self.passwordTextField.text == "" {
+                    self.showAlert(type: .error("IDとPasswordどちらも入力してください"))
+                } else {
+                    self.showAlert(type: .success)
+                    self.idLabel.text = "ID: \(self.idTextField.text ?? "")"
+                    self.passwordLabel.text = "Password: \(self.passwordTextField.text ?? "")"
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
 
