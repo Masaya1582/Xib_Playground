d37dd7453fd15acbfd98cc010adc32073c4f6b9f
Masaya Nakakuki <masa8y88@gmail.com>
2023-10-25 10:01:07 +0900

    Update Default

--- Xib_Playground/Sources/ViewControllers/HomeViewController.swift
+++ Xib_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -36,7 +36,7 @@ final class HomeViewController: UIViewController {
 
 }
 
-// MARK: - Binding
+// MARK: - Bind
 private extension HomeViewController {
     func bind(to viewModel: Dependency) {
 //        <#Button#>.rx.tap.asSignal()
