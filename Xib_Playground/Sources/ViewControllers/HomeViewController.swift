519f459ad2d484f2c7e2a0a0b4a33b127c9d0951
Masaya Nakakuki <masa8y88@gmail.com>
2023-11-22 16:23:18 +0900

    RxDataSourcesを学ぶ

--- Xib_Playground/Sources/ViewControllers/HomeViewController.swift
+++ Xib_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -11,42 +11,34 @@ import RxCocoa
 import RxDataSources
 
 final class HomeViewController: UIViewController {
-    // MARK: - Dependency
-    typealias Dependency = Void
-
     // MARK: - Properties
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.register(UINib(nibName: "HomeTableViewCell", bundle: nil), forCellReuseIdentifier: "HomeTableViewCell")
+        }
+    }
+
+    private let viewModel = HomeViewModel()
     private let disposeBag = DisposeBag()
-    private let viewModel: Dependency
-
-    // MARK: - Initialize
-    init(dependency: Dependency) {
-        self.viewModel = dependency
-        super.init(nibName: Self.className, bundle: Self.bundle)
-    }
-
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        bind(to: viewModel)
+        setupTableView()
     }
 
-}
+    private func setupTableView() {
+        let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, String>>(
+            configureCell: {(_, tableView, indexPath, item) in
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as? HomeTableViewCell else {
+                    return UITableViewCell()
+                }
+                cell.configure(with: item)
+                return cell
+            })
 
-// MARK: - Bind
-private extension HomeViewController {
-    func bind(to viewModel: Dependency) {
-//        <#Button#>.rx.tap.asSignal()
-//            .emit(onNext: { [weak self] in
-//                <#Actions#>
-//            })
-//            .disposed(by: disposeBag)
+        viewModel.sections
+            .drive(tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
     }
 }
-
-// MARK: - ViewControllerInjectable
-extension HomeViewController: ViewControllerInjectable {}
--- Xib_Playground/Sources/Views/TableViewCells/HomeTableViewCell.swift
+++ Xib_Playground/Sources/Views/TableViewCells/HomeTableViewCell.swift
@@ -12,14 +12,17 @@ import RxDataSources
 
 final class HomeTableViewCell: UITableViewCell {
     // MARK: - Properties
+    @IBOutlet private weak var titleLabel: UILabel!
 
     // MARK: - Initialize
     override func prepareForReuse() {
         super.prepareForReuse()
+        titleLabel.text = nil
     }
 
     // MARK: - Setup
-    func configure() {
+    func configure(with title: String) {
+        titleLabel.text = title
     }
     
 }
