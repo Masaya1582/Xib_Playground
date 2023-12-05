a52064da8e723731d48ca54c61ebc09eb6882721
Masaya1582 <masa8y88@gmail.com>
2023-12-05 20:03:09 +0900

    SearchBarのおさらい

--- Xib_Playground/Sources/Views/TableViewCells/HomeTableViewCell.swift
+++ Xib_Playground/Sources/Views/TableViewCells/HomeTableViewCell.swift
@@ -12,14 +12,17 @@ import RxDataSources
 
 final class HomeTableViewCell: UITableViewCell {
     // MARK: - Properties
+    @IBOutlet private weak var pokemonNameLabel: UILabel!
 
     // MARK: - Initialize
     override func prepareForReuse() {
         super.prepareForReuse()
+        pokemonNameLabel.text = nil
     }
 
     // MARK: - Setup
-    func configure() {
+    func configure(with pokemon: String) {
+        pokemonNameLabel.text = pokemon
     }
     
 }
