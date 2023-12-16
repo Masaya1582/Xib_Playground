1ec8b9c6c02697e00bfb0aad69aef62be737a611
Masaya1582 <masa8y88@gmail.com>
2023-12-17 08:08:16 +0900

    RxDataSourceの練習3

--- Xib_Playground/Sources/Views/TableViewCells/HomeTableViewCell.swift
+++ Xib_Playground/Sources/Views/TableViewCells/HomeTableViewCell.swift
@@ -12,14 +12,20 @@ import RxDataSources
 
 final class HomeTableViewCell: UITableViewCell {
     // MARK: - Properties
+    @IBOutlet private weak var subtitleLabel: UILabel!
+    @IBOutlet private weak var titleLabel: UILabel!
 
     // MARK: - Initialize
     override func prepareForReuse() {
         super.prepareForReuse()
+        titleLabel.text = nil
+        subtitleLabel.text = nil
     }
 
     // MARK: - Setup
-    func configure() {
+    func configure(title: String, subtitle: String) {
+        titleLabel.text = title
+        subtitleLabel.text = subtitle
     }
-    
+
 }
